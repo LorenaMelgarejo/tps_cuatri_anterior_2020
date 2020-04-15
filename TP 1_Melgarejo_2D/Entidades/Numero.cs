6686fa01641529eb7b73bfbbd6b65b07a6d39f30
Lorena Melgarejo <lorena_melgarejo@yahoo.com.ar>
2020-04-15 14:36:19 -0300

    TP1

---
+++ TP 1_Melgarejo_2D/Entidades/Numero.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Numero
+    {
+        private double numero;
+
+        /// <summary>
+        ///  asignará un valor al atributo número
+        /// </summary>
+        private string SetNumero
+        {
+
+            set { this.numero = this.ValidarNumero(value); }
+        }
+
+
+        #region Metodos
+
+        /// <summary>
+        /// constructor por defecto que inicializa numero en 0
+        /// </summary>
+
+        public Numero()
+        {
+            this.numero = 0;
+        }
+
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Numero(string strNumero)
+        {
+
+            this.SetNumero = strNumero;
+        }
+
+
+
+
+
+        public string BinarioDecimal(string binario)
+        {
+
+            int exponente = binario.Length - 1;
+            int num_decimal = 0;
+
+            for (int i = 0; i < binario.Length; i++)
+            {
+
+                if (int.Parse(binario.Substring(i, 1)) == 1)
+                {
+                    num_decimal = num_decimal + int.Parse(System.Math.Pow(2, double.Parse(exponente.ToString())).ToString());
+                }
+
+                exponente--;
+            }
+            return num_decimal.ToString();
+        }
+
+
+
+
+
+
+        public string DecimalBinario(double numero)
+        {
+            string bin = "";
+
+            if (numero > 0)
+            {
+                while (numero > 0)
+                {
+                    if (numero % 2 == 0)
+                    {
+                        bin = "0" + bin;
+                    }
+                    else
+                    {
+                        bin = "1" + bin;
+                    }
+                    numero = (int)numero / 2;
+                }
+            }
+            else if (numero == 0)
+            {
+                bin = "0";
+            }
+            return bin;
+        }
+
+
+        public string DecimalBinario(string numero)
+        {
+
+            string cadena = "";
+
+            cadena = this.DecimalBinario(Convert.ToDouble(numero));
+
+            return cadena;
+
+        }
+
+        public static double operator -(Numero n1, Numero n2)
+        {
+            return n1.numero - n2.numero;
+        }
+
+        public static double operator +(Numero n1, Numero n2)
+        {
+            return n1.numero + n2.numero;
+        }
+
+        public static double operator *(Numero n1, Numero n2)
+        {
+            return n1.numero * n2.numero;
+        }
+
+        public static double operator /(Numero n1, Numero n2)
+        {
+            return n1.numero / n2.numero;
+        }
+
+        /// <summary>
+        /// valida que el valor recibido sea numérico
+        /// </summary>
+        /// <param name="strNumero"></param>
+        /// <returns> retornará en formato double</returns>
+
+        private double ValidarNumero(string strNumero)
+        {
+            double valor;
+
+            if (double.TryParse(strNumero, out valor))
+            {
+                return valor;
+            }
+
+            else
+            {
+                return 0;
+            }
+
+        }
+        #endregion
+    }
+}
